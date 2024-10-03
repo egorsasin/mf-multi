9cb425a4af8213bd67f311434c5147aff9f91e18
Egor Sasin <egorsasin@gmail.com>
2024-10-03 22:39:52 +0200

    add host app

---
+++ apps/shell/src/app/app.tsx
@@ -0,0 +1,24 @@
+import * as React from 'react';
+
+import NxWelcome from './nx-welcome';
+
+import { Link, Route, Routes } from 'react-router-dom';
+
+import { loadRemoteModule } from '@nx/react/mf';
+
+export function App() {
+  return (
+    <React.Suspense fallback={null}>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+      </ul>
+      <Routes>
+        <Route path="/" element={<NxWelcome title="shell" />} />
+      </Routes>
+    </React.Suspense>
+  );
+}
+
+export default App;
